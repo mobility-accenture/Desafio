61c0a6dc989faecb8980085d02f57bbe017cdf34
Kaio Dantas <ikaioi@yahoo.com.br>
2018-09-03 12:11:19 -0300

    Criacao de projeto iOS com Xcode

---
+++ AppiOSProject/AppProject/AppProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppProject
+//
+//  Created by Kaio Dantas on 03/09/2018.
+//  Copyright © 2018 KaioDantas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
