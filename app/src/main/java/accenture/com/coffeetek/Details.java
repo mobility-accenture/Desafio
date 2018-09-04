862c6d173a384647dc6339bd907ff79dd7a8971a
Caio <caio.araujo@serttel.com.br>
2018-09-04 00:38:14 -0300

    - Design da tela Details, com todos as opções de seleção possíveis e para diferentes tamanhos de tela (caso a tela seja pequena demais e não caiba toda a informação, a tela fica rolável)

--- app/src/main/java/accenture/com/coffeetek/Details.java
+++ app/src/main/java/accenture/com/coffeetek/Details.java
@@ -1,12 +1,17 @@
 package accenture.com.coffeetek;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Details extends AppCompatActivity {
+
+    LinearLayout linear_Layout_Image_Details, linear_Layout_Information_Details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,33 @@ public class Details extends AppCompatActivity {
         Intent data = getIntent();
         int position = data.getIntExtra( "position", 0 );
 
-        TextView textView = (TextView) findViewById(R.id.details_text);
-        textView.setText( String.valueOf(position) );
+
+
+        int screenHeight, screenWidth;
+
+        //Code to determine screen's height and width.
+
+        Display display = getWindowManager().getDefaultDisplay();
+        if (android.os.Build.VERSION.SDK_INT>=13) {
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight = size.y;
+            screenWidth = size.x;
+        }
+        else {
+            screenWidth = display.getWidth();
+            screenHeight = display.getHeight();
+        }
+
+
+        linear_Layout_Image_Details = (LinearLayout) findViewById(R.id.linear_Layout_Image_Details);
+        linear_Layout_Information_Details = (LinearLayout) findViewById(R.id.linear_Layout_Information_Details);
+
+        linear_Layout_Image_Details.getLayoutParams().height = (screenHeight)/5;
+        linear_Layout_Information_Details.getLayoutParams().height = (screenHeight*3)/5;
+
+
+
 
 
 
